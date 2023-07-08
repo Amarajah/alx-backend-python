91c569f8b1c70218e8091572e9396eb2e90cb984
Amarajah <naomi.ibeh69@gmail.com>
2023-07-08 08:58:10 +0000

    floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""type-annotated function."""
+
+
+def floor(n: float) -> int:
+    """function which takes a float as argument and returns the floor."""
+    return math.floor(n)
