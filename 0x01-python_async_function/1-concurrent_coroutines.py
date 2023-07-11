cb702910e7b2aa43548b03dcc91244d666c84037
Amarajah <naomi.ibeh69@gmail.com>
2023-07-11 11:51:03 +0000

    coroutines

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
-"""more async."""
+""" 1-concurrent_coroutines """
 import asyncio
-import random
 from typing import List
-wait_random = __import__('0-basic_async_syntax.py').wait_random
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
-    """Asynchronous list."""
-    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
-    return [await task for task in asyncio.as_completed(tasks)]
+async def wait_n(n: int, max_delay: int) -> List[int]:
+    """return a list of float arrays. """
+    delays: List[float] = []
+    delay: Any = None
+    for i in range(n):
+        delay = await wait_random(max_delay)
+        if (delay is not None):
+            delays.append(delay)
+    return delays
