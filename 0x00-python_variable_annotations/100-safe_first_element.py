0ef69596756596822f74b7ec28dfc0f50ea325a3
Amarajah <naomi.ibeh69@gmail.com>
2023-07-20 09:45:03 +0000

    add annotations

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """The types of the elements of the input are not know."""
-from typing import Union, Optional, Iterable
+from typing import Union, Sequence, Any
 
 
-def safe_first_element(lst: Iterable) -> Optional:
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """Return the first element of a list or None if list is empty."""
     if lst:
         return lst[0]
     else:
--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 """Given the parameters and the return values,
 add type annotations to the function."""
-from typing import Union, Any, Mapping, TypeVar
+from typing import Union, Any, Mapping, TypeVar, Optional
+
 T = TypeVar('T')
 
 
 def safely_get_value(dct: Mapping, key: Any,
-                  / default: Union[T,  None] = None) -> Union[Any, T]:
+                  / default: Optional[T] = None) -> Union[Any, T]:
     if key in dct:
         return dct[key]
     else:
