f261150a9a3859e91d92f988fe90ce1fc9d35206
Amsrajah <naomi.ibeh69@gmail.com>
2023-08-20 23:10:58 +0000

    tests

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,25 +1,28 @@
 #!/usr/bin/env python3
-""" method to test that the method returns what it is supposed to."""
+"""
+access_nested_map function test
+"""
 import unittest
-from parameterized import parameterized
-from utils import access_nested_map
-from typing import import Mapping, Sequence, Any
-from utils import access_nested_map, get_json, memoize
 import requests
 from unittest.mock import patch
+from utils import access_nested_map, get_json, memoize
+from typing import Mapping, Sequence, Any
+from parameterized import parameterized
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """TestAccessNestedMap class that inherits from unittest.TestCase"""
+    """
+    Tests the access_nested_map function
+    """
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), ("b": 2)),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
-        ])
+    ])
     def test_access_nested_map(self, nested_map: Mapping,
                                path: Sequence, expected: int) -> None:
         """
-        Tests the access_nested_map method.
+        Test the above method
         """
         response = access_nested_map(nested_map, path)
         self.assertEqual(response, expected)
@@ -27,28 +30,33 @@ class TestAccessNestedMap(unittest.TestCase):
     @parameterized.expand([
         ({}, ("a",)),
         ({"a": 1}, ("a", "b"))
-        ])
+    ])
     def test_access_nested_map_exception(self, nested_map: Mapping,
                                          path: Sequence) -> None:
-    """
-    Test the access_nested_map method and raise an error if need be
-    """
-    with self.assertRaises(Exception):
-        access_nested_map(nested_map, path)
+        """
+        Test for the exception
+        """
+        with self.assertRaises(Exception):
+            access_nested_map(nested_map, path)
 
 
 class TestGetJson(unittest.TestCase):
-    """the utils.get_json function."""
-     @parameterized.expand([
-         ("http://example.com", {"payload": True}),
-         ("http://holberton.io", {"payload": False})
-         ])
-     def test_get_json(self, test_url, test_payload, mock_requests_get):
-         """tests"""
-         mock_requests_get.return_value.json.return_value = test_payload
-         result = get_json(test_url)
-         self.assertEqual(result, test_payload)
-         mock_requests_get.assert_called_once_with(test_url)
+    """
+    Test the get_json function
+    """
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    @patch("requests.get")
+    def test_get_json(self, test_url, test_payload, mock_requests_get):
+        """
+        Test the get_json method
+        """
+        mock_requests_get.return_value.json.return_value = test_payload
+        result = get_json(test_url)
+        self.assertEqual(result, test_payload)
+        mock_requests_get.assert_called_once_with(test_url)
 
 
 class TestMemoize(unittest.TestCase):
@@ -57,7 +65,7 @@ class TestMemoize(unittest.TestCase):
     """
     def test_memoize(self):
         """
-        Test that utils.memoize decorator works
+        Test the utils.memoize decorator
         """
         class TestClass:
 
@@ -67,9 +75,8 @@ class TestMemoize(unittest.TestCase):
             @memoize
             def a_property(self):
                 return self.a_method()
-
-         with patch.object(TestClass, 'a_method') as mock_object:
-             test = TestClass()
-             test.a_property()
-             test.a_property()
-             mock_object.assert_called_once()
+        with patch.object(TestClass, 'a_method') as mock_object:
+            test = TestClass()
+            test.a_property()
+            test.a_property()
+            mock_object.assert_called_once()
